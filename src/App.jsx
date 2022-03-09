d8055565dd28b7ce778784db98cf2621a8ab784f
jackXu <xujinzhang@372163.com>
2022-03-09 12:29:24 +0800

    layout init

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,17 @@ import { Outlet } from "react-router-dom";
 
 import "./App.css";
 
-export default function App() {
+import ActivityBar from "./pages/Layout/ActivityBar";
 
-  //窗口消息
-  var messageTop = function (){
-    new Notification("标题", { body: "消息窗口信息" })
-  }
+/**
+ * 基础布局
+ */
+export default function App() {
 
   return (
     <div>
-      <div className="titlBar">11111</div>
-      <input type="button" value="弹窗" onClick={messageTop} />
+      <div className="titlBar"></div>
+      <ActivityBar />
       <Outlet />
     </div>
   );
---
+++ src/pages/Layout/ActivityBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function ActivityBar() {
+  return (
+    <div className='activityBar'>
+        <ul>
+            <li><span className='iconfont icon-file-add'></span></li>
+        </ul>
+    </div>
+  )
+}

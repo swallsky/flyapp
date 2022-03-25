d9999c39d660f945d5f02e92142ce44cf7bea3ca
sky <xjz1688@163.com>
2022-03-25 12:10:21 +0800

    ui fixed

--- src/win/WinApp.jsx
+++ src/win/WinApp.jsx
@@ -1,8 +1,10 @@
+import { FileAddOutlined, ProfileOutlined } from '@ant-design/icons';
+import './styles/app.css';
 import React from "react";
+import { Outlet } from 'react-router-dom';
+import { Layout, Menu } from "antd";
+const { Content, Sider } = Layout;
 
-
-import "./styles/App.css";
-import Layout from "./pages/Layout/Layout";
 /**
  * 基础布局
  */
@@ -11,7 +13,29 @@ export default function WinApp() {
   return (
     <div>
       <div className="titlBar"></div>
-      <Layout />
+      <Layout hasSider
+        style={{
+          overflow: 'auto',
+          height: '100vh',
+          position: 'fixed',
+          left: 0,
+          top: 30,
+          right: 0,
+          bottom: 0,
+        }}
+      >
+        <Sider
+          width={60}
+        >
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" style={{paddingLeft:'16px'}} icon={<FileAddOutlined style={{fontSize:28}} />}></Menu.Item>
+            <Menu.Item key="2" style={{paddingLeft:'16px'}} icon={<ProfileOutlined style={{fontSize:28}} />}></Menu.Item>
+          </Menu>
+        </Sider>
+        <Content>
+          <Outlet />
+        </Content>
+      </Layout>
     </div>
   );
 }

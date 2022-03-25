afe60d41917610b6def836295b862f1f454bd8bb
sky <xjz1688@163.com>
2022-03-25 13:07:02 +0800

    add file fixed

--- src/server/SerApp.jsx
+++ src/server/SerApp.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-
-
 import "./styles/App.css";
-import Layout from "./pages/Layout/Layout";
+import { Outlet } from 'react-router-dom';
+import React from "react";
+import { Layout, Menu } from "antd";
+const { Header, Content } = Layout;
 /**
  * 基础布局
  */
 export default function SerApp() {
 
   return (
-    <div>
-      <Layout />
-    </div>
+    <Layout className="layout">
+      <Header>
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">文件上传</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ height:'100vh' }}>
+        <Outlet />
+      </Content>
+    </Layout>
   );
 }

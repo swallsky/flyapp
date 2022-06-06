744ba5707e863f374dcd4b00dc4db21dcbbafedc
jackXu <xujinzhang@372163.com>
2022-06-06 14:06:51 +0800

    production env add fixed

--- src/preload/main.js
+++ src/preload/main.js
@@ -6,11 +6,32 @@ const remote = require("@electron/remote");
 const { chromium } = require("playwright");
 const acWeb = require("./acWeb");
 var browser = null;
-chromium.launch({ headless:false }).then(res=>{
+chromium.launch({ headless: false }).then((res) => {
   browser = res;
 });
 
 contextBridge.exposeInMainWorld("electronApi", {
+  // 生产环境
+  production: function (e) {
+    if (remote.app.isPackaged === true) { //正式打包环境下，禁止使用以下快捷键
+      e = window.event || e;
+      let k = e.keyCode;
+      //屏蔽ctrl+R,F5，ctrl+F5 F3,mac下command+R,command+a,alt+command+i
+      if (
+        (e.metaKey === true && k === 82) ||
+        (e.metaKey === true && k === 65) ||
+        (e.altKey === true && e.metaKey === true && k === 73) ||
+        k === 116 ||
+        (e.ctrlKey === true && k === 116) ||
+        k === 114
+      ) {
+        e.keyCode = 0;
+        e.returnValue = false;
+        e.cancelBubble = true;
+        return false;
+      }
+    }
+  },
   // 获取本机ip
   getLocalIP: async () => ipcRenderer.invoke("get-local-ip"),
   //保存文件夹
@@ -34,11 +55,12 @@ contextBridge.exposeInMainWorld("electronApi", {
    */
   webapp: async (data) => {
     data.wtype = data.wtype.replace("web,", ""); //替换web,前缀
-    try{//容错处理，防止因为浏览器关闭，导致的错误
-      await acWeb(browser,data);
-    }catch(e){
-      browser = await chromium.launch({ headless:false });
-      await acWeb(browser,data);
+    try {
+      //容错处理，防止因为浏览器关闭，导致的错误
+      await acWeb(browser, data);
+    } catch (e) {
+      browser = await chromium.launch({ headless: false });
+      await acWeb(browser, data);
     }
   },
 });
